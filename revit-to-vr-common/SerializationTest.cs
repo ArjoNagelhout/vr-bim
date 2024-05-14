068239703f47e8ce22e297e1b0d4156d5cc6b50d
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2024-05-14 16:37:50 +0200

    add revit-to-vr-common

---
+++ revit-to-vr-common/SerializationTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace revit_to_vr_common
+{
+    enum EventType
+    {
+        A,
+        B,
+        C
+    }
+
+    [System.Serializable]
+    public struct NestedData
+    {
+        int a;
+        int b;
+        int c;
+        bool yes;
+        bool no;
+    }
+
+    [System.Serializable]
+    public struct TestData
+    {
+        EventType eventType;
+        float someValue;
+        string anotherValue;
+        NestedData nestedData;
+        NestedData nestedData2;
+    }
+}
